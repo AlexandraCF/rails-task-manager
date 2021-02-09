2e5a41a77fdc405558bb54b274e2133be528503d
Alexandra Canuel <alexcanuel@gmail.com>
2021-02-09 15:27:54 +0100

    task manager CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
 	end
 
 	def show
-		@task=Task.find(params[:id])
+		@task = Task.find(params[:id])
 	end
 
 	def new
@@ -16,6 +16,23 @@ class TasksController < ApplicationController
 		@task.save
 		redirect_to task_path(@task)
 	end
+
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+		redirect_to task_path(@task)
+	end
+
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		redirect_to tasks_path
+	end
+
 private
 
   def task_params
